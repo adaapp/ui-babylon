b2a92fa8e91d0479cc4b527aef1d1363b0979889
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-08-08 11:08:11 +0100

    Add new message handler

--- babylon-react/src/pages/Chatbot.js
+++ babylon-react/src/pages/Chatbot.js
@@ -1,24 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 
-function Chatbot() {
+function Chatbot(props) {
+  
+  let initialMessages = [
+    { userType: "bot", content: "Hi Laurie, how can I help you?" },
+    { userType: "user", content: props.initialMessage },
+    {
+      userType: "bot",
+      content: (
+        <span class="typing">
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </span>
+      )
+    }
+  ]
+
+  let [messages, setMessages] = useState(initialMessages)
+
+  function handleNewMessage(event) {
+    event.preventDefault()
+    let newMessage = event.target[0].value
+    let newMessageObj = { userType: "user", content: newMessage}
+    let newMessages = messages.slice(0)
+    newMessages.push(newMessageObj)
+    setMessages(newMessages)
+    event.target[0].value = ''
+    window.scrollTo(0, 100000)
+  }
+
   return (
-    <section id="chatbot" class="page">
+    <section id="chatbot" className="page swoosh">
       <header>
         <img class="logo" alt="Babylon" src="img/babylon-logo.png" />
       </header>
-      <article>
-        <p class="bubble bot">Hi Laurie, how can I help you?</p>
-        <p class="bubble user">I have a sore throat.</p>
-        <p class="bubble bot">
-          <span class="typing">
-            <span>.</span>
-            <span>.</span>
-            <span>.</span>
-          </span>
-        </p>
+      <article class="clearfix">
+        { messages.map(m => (
+          <p className={`bubble ${m.userType}`}>
+            {m.content}
+          </p>
+        )) }
       </article>
       <footer>
-        <form>
+        <form onSubmit={handleNewMessage}>
           <input type="text" placeholder="Type a message" />
         </form>
       </footer>
